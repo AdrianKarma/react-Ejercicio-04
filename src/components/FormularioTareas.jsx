bd51a9aeabfa5a04846b28ebd714ffc4c62968bb
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-01-20 20:07:23 -0300

    agregamos un state nuevo tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,15 +1,21 @@
 import { Button , Form} from 'react-bootstrap';
+import { useState } from 'react';
 //import Form from 'react-bootstrap/Form';
 import ListaTareas from './ListaTareas';
 
 
 const FormularioTareas = () => {
+const [tarea, setTarea] = useState("");
+const [tareas, setTareas] = useState([]);
+
     return (
         <section>
             <Form>
       <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
         <Form.Label>Ingresa tus Tareas:</Form.Label>
-        <Form.Control type="text" placeholder="Tarea1" minLength={3} maxLength={50} />
+        <Form.Control type="text" placeholder="Tarea1" minLength={3} maxLength={50} onChange={(e)=> setTarea(e.target.value)} 
+        value={tarea}
+        />
         <Button className='mx-3' type='submit' variant='info'>Enviar</Button>
       </Form.Group>
       
