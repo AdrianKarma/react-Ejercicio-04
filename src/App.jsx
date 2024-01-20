e4ff755d546b32309252ffd3bab8961b4452d5b2
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-01-20 19:15:31 -0300

    bajar el footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormularioTareas from './components/FormularioTareas';
 import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
 
 function App() {
   
 
   return (
-    <Container className='my-4'>
+    <>
+    <section className='prueba'>
+    <Container className='my-4 prueba2'>
       <h1 className='text-center display-3'>Lista de Tareas</h1>
       <FormularioTareas></FormularioTareas>
     </Container>
+    <Footer ></Footer>
+    </section>
+    </>
   )
 }
 
